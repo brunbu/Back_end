0d52dd7e415bc921e2fd90154a20cb41936f2a1e
Bruno <bu9836564@gmail.com>
2025-10-28 08:37:13 -0300

    Exercicos e Desafios

---
+++ AgenciaBancaria/Program.cs
@@ -0,0 +1,143 @@
+﻿ Console.Clear();
+string[] nomes = new String[3];
+double[] saldos = new double[3];
+int totalClientes = 0;
+int opcao = -1;
+
+
+do
+{
+    Console.WriteLine("====================================");
+    Console.WriteLine("$         Bem vindo    ");
+    Console.WriteLine("$             Ao    ");
+    Console.WriteLine("$    Sistema da Agencia Bancaria    ");
+    Console.WriteLine("====================================");
+    Console.WriteLine("");
+
+    Console.WriteLine("===============================");
+    Console.WriteLine($"Escolha uma das opções abaixo:");
+    Console.WriteLine($"1- Cadastrar Cliente");
+    Console.WriteLine($"2- Depositar");
+    Console.WriteLine($"3- Sacar");
+    Console.WriteLine($"4- Transferir");
+    Console.WriteLine($"5- Listar Clientes");
+    Console.WriteLine($"0- Sair");
+    Console.WriteLine($"==============================");
+    opcao = int.Parse(Console.ReadLine());
+    Console.WriteLine("");
+
+    switch (opcao)
+    {
+        case 0:
+            Sair();
+            break;
+        case 1:
+            CadastrarCliente();
+            break;
+        case 2:
+            Depositar();
+            break;
+        case 3:
+            Sacar();
+            break;
+        case 4:
+            Transferir();
+            break;
+        case 5:
+            ListarClientes();
+            Console.WriteLine($"Pressione <Enter> para continuar...");
+            Console.ReadLine();
+            break;
+        default:
+            Console.WriteLine("ERRO: A opção escolhida não existe.");
+            break;
+    }
+} while (opcao != 0);
+
+void CadastrarCliente()
+{
+    if (totalClientes >= 3)
+    {
+        Console.WriteLine("Limite de clientes atingido");
+        Console.WriteLine($"Pressione <Enter> para encerrar...");
+        Console.ReadLine();
+        return;
+    }
+    Console.WriteLine("Digite o nome do Cliente");
+    nomes[totalClientes] = Console.ReadLine();
+    saldos[totalClientes] = 0;
+    totalClientes++;
+
+    Console.WriteLine("Cliente cadastrado com sucesso");
+    Console.WriteLine($"Pressione <Enter> para continuar...");
+    Console.ReadLine();
+}
+void Depositar()
+{
+    Console.WriteLine($"Qual cliente deseja fazer o deposito: ");
+
+    ListarClientes();
+    int n = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite o quanto deseja depositar");
+    saldos[n - 1] += double.Parse(Console.ReadLine());
+    Console.WriteLine("Saldo depositado com sucesso");
+
+    Console.WriteLine($"O saldo atual de {nomes[n - 1]} é de R${saldos[n - 1]}");
+    Console.WriteLine($"Pressione <Enter> para continuar...");
+    Console.ReadLine();
+}
+void Sacar()
+{
+    Console.WriteLine($"Qual cliente deseja fazer o saque: ");
+
+    ListarClientes();
+    int n = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite o quanto deseja sacar");
+    saldos[n - 1] -= double.Parse(Console.ReadLine());
+    Console.WriteLine("Saldo sacado com sucesso");
+
+    Console.WriteLine($"O saldo atual de {nomes[n - 1]} é de R${saldos[n - 1]}");
+    Console.WriteLine($"Pressione <Enter> para continuar...");
+    Console.ReadLine();
+}
+void Transferir()
+{
+    Console.WriteLine($"Qual cliente deseja Transferir: ");
+    ListarClientes();
+    int n = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite o quanto deseja transferir");
+    double transferir = double.Parse(Console.ReadLine());
+    saldos[n - 1] -= transferir;
+
+    Console.WriteLine($"Qual cliente irá receber a transferência : ");
+    ListarClientes();
+    int t = int.Parse(Console.ReadLine());
+    saldos[t - 1] += transferir;
+    Console.WriteLine("Saldo transferido com sucesso com sucesso");
+
+    Console.WriteLine($"O saldo atual de {nomes[t - 1]} é de R${saldos[t - 1]}");
+    Console.WriteLine($"O saldo atual de {nomes[n - 1]} é de R${saldos[n - 1]}");
+    Console.WriteLine($"Pressione <Enter> para continuar...");
+    Console.ReadLine();
+}
+void ListarClientes()
+{
+    for (int i = 0; i < totalClientes; i++)
+    {
+        Console.WriteLine($"{i + 1}- {nomes[i]} | Saldo atual: R${saldos[i]} ");
+    }
+
+}
+void Sair()
+{
+    Console.WriteLine("=======================================");
+    Console.WriteLine("      Obrigado por usar Até mais       ");
+    Console.WriteLine("=======================================");
+}
+
+
+
+
--- ReviewArrays/Program.cs
+++ ReviewArrays/Program.cs
@@ -1,84 +1,89 @@
-﻿Console.Clear();//limpa tela
-Console.WriteLine($"Digite seu nome:");//pede o nome do usuário
-string nomeUsuario = Console.ReadLine();//guarda o nome na variável
-Cumprimento(nomeUsuario);//chama a função e passa o nome
+﻿using System.Net.WebSockets;
+using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics.Arm;
+Console.Clear();
+string[] nomes = new string[4];
+int[] idades = new int[4];
+int opcao = -1, totalAlunos = 0;
 
 
-int opcao = -1;
+// nomes[0] = "Maria";
+// idades[0] = 17;
+// nomes[1] = "Matheus";
+// idades[1] = 16;
+// nomes[2] = "Eloysa";
+// idades[2] = 17;
+// nomes[3] = "Felipe";
+// idades[3] = 16;
 
-
-void opcao1()
-{
-    Console.WriteLine($"Você escolheu a opção 1 - Coxinha");
-}
-
-void opcao2()
-{
-    Console.WriteLine($"Você escolheu a opção 2 - Pastel");
-}
-
-void opcao3()
-{
-    Console.WriteLine($"Você escolheu a opção 3 - Caldo de Cana");
-}
-
-void opcao4()
-{
-    Console.WriteLine($"Você escolheu a opção 4 - Bolo de Chocolate");
-}
-
-void opcao0()
-{
-    Console.WriteLine($"Você escolheu sair - Obrigado por usar o programa");
-}
-
-void opcaoinvalida()
-{
-    Console.WriteLine($"Não temos esta opção {opcao} !!");
-}
 
 do
-{//todo o programa aqui dentro
- //desenha um menu de opções
-    Console.WriteLine("Escolha uma opção no menu abaixo:");
-    Console.WriteLine(" 1 - Coxinha");
-    Console.WriteLine(" 2 - Pastel");
-    Console.WriteLine(" 3 - Caldo de Cana");
-    Console.WriteLine(" 4 - Bolo de Chocolate");
-    Console.WriteLine(" 0 - Sair");
-    //captura a opção do usuário
+{
+    Console.WriteLine($"1) Cadastrar Aluno");
+    Console.WriteLine($"2) Listar Aluno");
+    Console.WriteLine($"0) Sair");
+    Console.WriteLine($"Digite um opção");
     opcao = int.Parse(Console.ReadLine());
+
 
     switch (opcao)
     {
+        case 0:
+            Console.WriteLine($"Encerrando ...");
+            Console.WriteLine($"Pressione <Enter> para encerrar ...");
+            Console.ReadLine();
+            break;
         case 1:
-            opcao1();
+            CadastrarAluno();
             break;
         case 2:
-            opcao2();
-            break;
-        case 3:
-            opcao3();
-            break;
-        case 4:
-            opcao4();
-            break;
-        case 0:
-            opcao0();
+            ListarAlunos();
             break;
         default:
-            opcaoinvalida();
+            Console.WriteLine($"opção invalida, pressione <Enter> para continuar ...");
+            Console.ReadLine();
             break;
     }
-} while (opcao != 0) ;
 
-Console.Clear();
-Console.WriteLine($"FIM DO PROGRAMA");
+} while (opcao != 0);
 
-
-
-// tipoDeDado Nome (parâmetros){corpo}
-void Cumprimento(string n)
+void CadastrarAluno()
 {
-    Console.WriteLine($"Olá, {n}");
+    if (totalAlunos >= 4)
+    {
+        Console.WriteLine($"Não Limite de ocupação atingindo");
+        Console.WriteLine($"Pressione <Enter> para continuar");
+        return;
+    }
+    Console.WriteLine($"Digite o nome do aluno");
+    string n = Console.ReadLine();
+    Console.WriteLine($"Digite a idade de {n}");
+    int i = int.Parse(Console.ReadLine());
+
+    nomes[totalAlunos] = n;
+    idades[totalAlunos] = i;
+    totalAlunos++;
+
+    Console.WriteLine($"Alunos cadastrado com sucesso");
+    Console.WriteLine($"Há {totalAlunos} alunos(s) cadastrado(s) no sistema!");
+    Console.WriteLine($"Pressione <Enter> para continuar");
+    Console.ReadLine();
+
+}while (opcao != 0);
+
+
+
+//
+void ListarAlunos()
+{
+Console.WriteLine();
+Console.WriteLine($"Resultado: ");
+
+    for (int i = 0; i < nomes.Length; i++)
+    {
+        Console.WriteLine($"    Nome:{nomes[i]}");
+        Console.WriteLine($"    idade: {idades[i]} anos");
+        Console.WriteLine();//pula linha
+    }
+
 }
